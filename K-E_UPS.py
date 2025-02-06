6211451050cea244674b34d324867e32db01e9b7
MahammadHashim786 <161362132+MahammadHashim786@users.noreply.github.com>
2025-02-06 08:23:44 +0500

    K_Electric and ups program

---
+++ K-E_UPS.py
@@ -0,0 +1,10 @@
+K_E_ups = int(input("Enter any current: "))
+
+if K_E_ups == 1:
+    print("K_Electric on")
+
+elif K_E_ups == 2:
+    print("Ups on")
+
+else:
+    print("wrong value.. Enter correct only 1 and 2 will be Accepted")

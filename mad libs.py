f3d7cd4b645352c1efc50da673d6d973f45ae3c4
MahammadHashim786 <161362132+MahammadHashim786@users.noreply.github.com>
2025-02-06 07:52:18 +0500

    mad libs

---
+++ mad libs.py
@@ -0,0 +1,6 @@
+number = input("Enter your name ")
+mod_to_fri = input("Enter whate you do ")
+fav_food = input("fav food ")
+ 
+print(f"""My Name is {number}\n Monday to Friday what i do is {mod_to_fri}\n
+ My favuorite food is {fav_food}""" )

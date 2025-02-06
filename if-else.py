801aed5c567ae6e2e9aaddb5bbee6c96e4bedc1a
MahammadHashim786 <161362132+MahammadHashim786@users.noreply.github.com>
2025-02-06 08:21:50 +0500

    if_else_code

---
+++ if-else.py
@@ -0,0 +1,19 @@
+if (23 == 23):
+    print("same to same")
+else:
+    print("confirmation Fasle")
+
+if (22 > 5):
+    print("Hello world")
+    uname = "Hashim"
+    print(uname)
+else:
+    print("No correct")
+    
+
+if (60 < 21):
+    print("correct")
+else:
+    print("Not correct")
+
+    
